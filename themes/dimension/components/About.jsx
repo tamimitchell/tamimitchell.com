6dd3fdc131f0195e6a1064e7e9af4e3470339447
Tami Mitchell <mail@tameramitchell.com>
2018-05-03 09:17:59 -0600

    About section

--- themes/dimension/components/About.jsx
+++ themes/dimension/components/About.jsx
@@ -14,13 +14,26 @@ export class About extends React.Component{
       'dimensions-hide': !this.props.class,
     });
 
-
     return (
       <article id="about" className={aboutClass}>
-        <h2 className="major">About</h2>
-        <span className="image main"><img src="images/pic03.jpg" alt="" /></span>
-        <p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. By the way, check out my <a href="#work">awesome work</a>.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
+        <h2>Hello! I'm Tami Mitchell</h2>
+        <span className="image right"><img src="../images/pic01.jpg" alt="" /></span>
+        <p>
+          I'm a programmer. I've worked as a remote Ruby on Rails and front end developer for 6 years.
+        </p>
+
+        <p>
+          I strive to build well crafted software, because I believe that having a mindful, craftsman-like mentality produces fulfilling results for clients and my own life. I also believe in the importance of shipping usable products and continually improving them over time - the prettiest code does nothing for the world if it never goes live.
+        </p>
+
+        <p>
+          I love to learn and be constantly growing into the best version of myself. I chose this career in large part because of the many opportunities to learn technical skills as well as the other vital skills for a remote programmer - empathy, design, communication and self motivation.
+        </p>
+
+        <p>
+          I believe being a good programmer also depends on having a healthy work-life balance. When I’m not coding I travel, hike, read, do yoga, go off-roading, spend quality time with friends and family, and practice my pizza-making-and-eating skills. I write about my &nbsp;
+          <a href="https://blog.tamimitchell.com">travels and personal life here.</a>
+        </p>
         <div onClick={() => this.props.endTransitionHandler('about')} className="close">Close</div>
       </article>
     );
--- themes/dimension/components/Contact.jsx
+++ themes/dimension/components/Contact.jsx
@@ -36,12 +36,6 @@ export class Contact extends React.Component{
             <li><input type="reset" value="Reset" /></li>
           </ul>
         </form>
-        <ul className="icons">
-          <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-          <li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
-          <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
-          <li><a href="#" className="icon fa-github"><span class="label">GitHub</span></a></li>
-        </ul>
         <div onClick={() => this.props.endTransitionHandler('contact')} className="close">Close</div>
       </article>
     );
